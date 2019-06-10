9a1d08642597de8dbea09d7cb2a6555f1d416bee
Thinh Tran <thinhtran@outlook.com.au>
2019-06-10 22:35:00 +0800

    adding mock data

---
+++ AusPetAdoption/Models/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace AusPetAdoption.Models
+{
+
+    public class Pet
+    {
+        public string PostTitle { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Breed { get; set; }
+
+        public string ImageUrl { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Size { get; set; }
+        public string Location { get; set; }
+
+    }
+}
---
+++ AusPetAdoption/Services/SamplePetData.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AusPetAdoption.Models;
+namespace AusPetAdoption.Services
+{
+    public static class SamplePetData
+    {
+        static Random random = new Random();
+        static T Random<T>(this IList<T> This) => This[random.Next(This.Count)];
+        static bool FlipCoin() => random.Next(1) == 0;
+
+        public static IList<Pet> Pets { get; private set; }
+
+
+        //credit: https://en.wikipedia.org/wiki/List_of_dog_breeds
+        static SamplePetData()
+        {
+            Pets = new List<Pet>();
+
+            Pets.Add(new Pet
+            {
+                PostTitle = "",
+                Description = "",
+                Breed = "",
+                ImageUrl = "",
+                DateOfBirth = DateTime.UtcNow.AddDays( - new Random().Next(1000)),
+                Size = sizes.Random(),
+                Location = "",
+            });
+
+            Pets.Add(new Pet
+            {
+                PostTitle = "",
+                Description = "",
+                Breed = "",
+                ImageUrl = "",
+                DateOfBirth = DateTime.UtcNow.AddDays(-new Random().Next(1000)),
+                Size = sizes.Random(),
+                Location = "",
+            });
+
+            Pets.Add(new Pet
+            {
+                PostTitle = "",
+                Description = "",
+                Breed = "",
+                ImageUrl = "",
+                DateOfBirth = DateTime.UtcNow.AddDays(-new Random().Next(1000)),
+                Size = sizes.Random(),
+                Location = "",
+            });
+
+            Pets.Add(new Pet
+            {
+                PostTitle = "",
+                Description = "",
+                Breed = "",
+                ImageUrl = "",
+                DateOfBirth = DateTime.UtcNow.AddDays(-new Random().Next(1000)),
+                Size = sizes.Random(),
+                Location = "",
+            });
+
+            Pets.Add(new Pet
+            {
+                PostTitle = "",
+                Description = "",
+                Breed = "",
+                ImageUrl = "",
+                DateOfBirth = DateTime.UtcNow.AddDays(-new Random().Next(1000)),
+                Size = sizes.Random(),
+                Location = "",
+            });
+
+            Pets.Add(new Pet
+            {
+                PostTitle = "",
+                Description = "",
+                Breed = "",
+                ImageUrl = "",
+                DateOfBirth = DateTime.UtcNow.AddDays(-new Random().Next(1000)),
+                Size = sizes.Random(),
+                Location = "",
+            });
+
+            Pets.Add(new Pet
+            {
+                PostTitle = "",
+                Description = "",
+                Breed = "",
+                ImageUrl = "",
+                DateOfBirth = DateTime.UtcNow.AddDays(-new Random().Next(1000)),
+                Size = sizes.Random(),
+                Location = "",
+            });
+
+            Pets.Add(new Pet
+            {
+                PostTitle = "",
+                Description = "",
+                Breed = "",
+                ImageUrl = "",
+                DateOfBirth = DateTime.UtcNow.AddDays(-new Random().Next(1000)),
+                Size = sizes.Random(),
+                Location = "",
+            });
+
+            Pets.Add(new Pet
+            {
+                PostTitle = "",
+                Description = "",
+                Breed = "",
+                ImageUrl = "",
+                DateOfBirth = DateTime.UtcNow.AddDays(-new Random().Next(1000)),
+                Size = sizes.Random(),
+                Location = "",
+            });
+
+            Pets.Add(new Pet
+            {
+                PostTitle = "",
+                Description = "",
+                Breed = "",
+                ImageUrl = "",
+                DateOfBirth = DateTime.UtcNow.AddDays(-new Random().Next(1000)),
+                Size = sizes.Random(),
+                Location = "",
+            });
+
+
+
+        }
+
+
+
+
+        static string[] sizes = new string[]
+        {
+            "Extra Small", "Small", "Medium", "Large", "Extra Large"
+        };
+    }
+}
+
